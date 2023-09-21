28adfa49c1a243df187e076fa5f66a7e5562d7f0
Seamus Cranley <cranley-seamus@bah.com>
2023-09-21 14:51:35 -0700

    addedfile

---
+++ Solutions/_AGENDA/DE - Data Engineering with Databricks.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+# MAGIC
+# MAGIC %md-sandbox
+# MAGIC
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+# MAGIC </div>
+
+# COMMAND ----------
+
+# DBTITLE 0,--i18n-6b534af6-7c1a-4524-b8a3-5e350e4f9602
+# MAGIC %md
+# MAGIC
+# MAGIC # Data Engineering with Databricks
+# MAGIC
+# MAGIC The Data Engineering with Databricks (DEWD) course is designed to prepare students for the Databricks Certified Associate Data Engineer certification exam. The content for this course consists of the Associate-level modules of the Data Engineer Learning Path, and can be delivered as an instructor-led training (ILT) or self-paced (SP) course.
+# MAGIC
+# MAGIC ## Course Agenda
+# MAGIC The following modules are part of the Data Engineer Learning Path by Databricks Academy.
+# MAGIC
+# MAGIC | # | Module Name |
+# MAGIC | --- | --- |
+# MAGIC | 0 | [Get Started with Databricks Data Science and Data Engineering Workspace]($../DE 1 - Databricks Workspace/DE 1.0 - Module Introduction) |
+# MAGIC | 1 | [Transform Data with Spark]($../DE 2 - ETL with Spark/DE 2.0 - Module Introduction) |
+# MAGIC | 2 | [Manage Data with Delta Lake]($../DE 3 - Delta Lake/DE 3.0 - Module Introduction) |
+# MAGIC | 3 | [Build Data Pipelines with Delta Live Tables]($../DE 4 - Delta Live Tables/DE 4.0 - Module Introduction) |
+# MAGIC | 4 | [Deploy Workloads with Databricks Workflows]($../DE 5 - Workflow Jobs/DE 5.0 - Module Introduction) |
+# MAGIC | 5 | [Manage Data Access for Analytics with Unity Catalog]($../DE 6 - Managing Permissions/DE 6.0 - Module Introduction) |
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC &copy; 2023 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/>
+# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="https://help.databricks.com/">Support</a>
